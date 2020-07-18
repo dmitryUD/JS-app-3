f8093962a133c2d1fb8a3751bc173b94e1eb956c
Dmitry Udovik <udovikdmitry@bk.ru>
2020-07-18 13:11:01 +0300

    Работа с табами и таймером

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    //Tabs
+    // получение элементов
     const tabs = document.querySelectorAll('.tabheader__item'), // табы
         tabsContent = document.querySelectorAll('.tabcontent'), // контент - изображение и текст
         tabsParent = document.querySelector('.tabheader__items'); // родитель табов (обертка)
 
     function hideTabContent() { // скрытие контента
         tabsContent.forEach(item => {
-            item.classList.add('hide'); // или item.style.block = "none"
+            item.classList.add('hide'); // или item.style.display = "none"
             item.classList.remove('show', 'fade');
         });
 
-        tabs.forEach(item => {
+        tabs.forEach(item => { // удаление класса активности у каждого таба
             item.classList.remove('tabheader__item_active');
         });
     }
 
+    // показ табов
     function showTabContent(i = 0) { // по стаднарту ES6. Если ф-я вызывается без аргумента, то по умолч. вставится 0
         tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
@@ -36,4 +38,57 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer
+    const deadline = '2020-07-27';
+
+    function getTimeRemainig(endtime) { // определяет разницу между дедлайном и текущем временем
+        const t = Date.parse(endtime) - Date.parse(new Date()), // parse - превращение в число
+              days = Math.floor(t / (1000*60*60*24)), // округление до ближайшего целого
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)), // 
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        
+        return { // возврат объекта наружу для использования
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+
+    function getZero(num) { // 08
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) { // установка таймера на страницу
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); // избежание мегания
+
+        function updateClock() { // обновление таймера каждую секунду
+            const t = getTimeRemainig(endtime); // расчет оставшегося времени, здесь находится объект!
+            // помещение на страницу
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) { // остановка таймера
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
